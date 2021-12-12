ee511f53eb0bc85724d9629142182a6d26b8dde9
Sebastian Baier HomePC <sebastian.baier93@hotmail.de>
2021-12-13 00:30:02 +0100

    Adds seed increase per timeStep to the sample scene

--- Unity Sampling/Assets/Examples/SamplesGizmoDrawer.cs
+++ Unity Sampling/Assets/Examples/SamplesGizmoDrawer.cs
@@ -20,19 +20,48 @@ namespace SBaier.Sampling.Examples
         private GameObject _connectionObject;
         [SerializeField]
         private int _retries = 10;
+        [SerializeField]
+        private int _seed = 0;
+        [SerializeField]
+        private float _secondsTillSeedIncrease = 2;
 
         private List<Vector3> _samples = new List<Vector3>();
-
+        private List<GameObject> _sampleObjects = new List<GameObject>();
+        private List<GameObject> _connections = new List<GameObject>();
 
         private void Start()
 		{
-            _samples = new PoissonDiskSampling3D(new System.Random(0), _retries).Sample(new PoissonDiskSampling3D.Parameters(_samplesAmount, _minimalDistance, _bounds, _bounds/2));
+            Create();
+            InvokeRepeating(nameof(Recreate), _secondsTillSeedIncrease, _secondsTillSeedIncrease);
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = new Color(1, 0, 0, 0.25f);
+            Gizmos.DrawCube(_bounds / 2, _bounds);
+        }
+
+        private void Recreate()
+		{
+            Debug.Log("Recreate");
+            _seed++;
+            Clear();
+            Create();
+		}
+
+        private void Create()
+		{
+            CreateSamples();
+            CreateConnections();
+        }
+
+        private void CreateSamples()
+		{
+            _samples = new PoissonDiskSampling3D(new System.Random(_seed), _retries).Sample(new PoissonDiskSampling3D.Parameters(_samplesAmount, _minimalDistance, _bounds, _bounds / 2));
             foreach (Vector3 sample in _samples)
             {
-                Instantiate(_sampleObject, sample, Quaternion.identity);
+                _sampleObjects.Add(Instantiate(_sampleObject, sample, Quaternion.identity, transform));
             }
-
-            CreateConnections();
         }
 
 		private void CreateConnections()
@@ -47,17 +76,22 @@ namespace SBaier.Sampling.Examples
                     float distance = (first - second).magnitude;
                     if (distance > _maximalDistance)
                         continue;
-                    Transform connection = Instantiate(_connectionObject, first, Quaternion.identity).transform;
+                    Transform connection = Instantiate(_connectionObject, first, Quaternion.identity, transform).transform;
                     connection.LookAt(second);
                     connection.localScale = new Vector3(1, 1, distance);
+                    _connections.Add(connection.gameObject);
                 }
             }
         }
 
-		private void OnDrawGizmos()
+        private void Clear()
 		{
-            Gizmos.color = new Color(1, 0, 0, 0.25f);
-            Gizmos.DrawCube(_bounds / 2, _bounds);
-		}
+            foreach (GameObject gO in _connections)
+                Destroy(gO);
+            foreach (GameObject gO in _sampleObjects)
+                Destroy(gO);
+            _connections.Clear();
+            _sampleObjects.Clear();
+        }
 	}
 }
